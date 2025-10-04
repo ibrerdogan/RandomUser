4d93458cc0f10bfb15c68dad88e10b6b90c5dc2f
ibrerdogan <94644110+ibrerdogan@users.noreply.github.com>
2025-10-04 14:10:25 +0300

    added user request to view model

--- RandomUser/Modules/UserList/UserListViewModel.swift
+++ RandomUser/Modules/UserList/UserListViewModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+protocol UserListViewModelProtocol: AnyObject {
+    func updateList(with users: [User])
+}
+final class UserListViewModel {
+    private var apiClient: APIClient
+    weak var delegate: UserListViewModelProtocol?
+    weak var coordinator: UserListCoordinator?
+    var users = [User]()
+    init(apiClient: APIClient) {
+        self.apiClient = apiClient
+    }
+    
+    func fetchUsers(for page: Int) {
+        apiClient.request(endpoint: .randomUsers(page: page)) { [weak self] (result: Result<RandomUserResponseModel, Error>) in
+            guard let strongSelf = self else {return}
+            switch result {
+            case .success(let response):
+                strongSelf.delegate?.updateList(with: response.results)
+                strongSelf.users = response.results
+            case .failure(let error):
+                print("Error: \(error.localizedDescription)")
+            }
+        }
+    }
+}
